d955b0ec05f685103e533c2268b786c67c9ce20b
codingcat12345 <145919227+codingcat12345@users.noreply.github.com>
2023-12-22 12:29:51 +0800

    Add files via upload

--- rrtstar.py
+++ rrtstar.py
@@ -3,6 +3,7 @@ import random
 import numpy as np
 
 class Block(pygame.sprite.Sprite):
+    #this class define a pygame rectangle shape sprite 
     def __init__(self, width, height,xpos,ypos,color=(255,255,255)):
         super().__init__()#=pygame.sprite.Sprite.__init__(self)
         self.rect=pygame.Rect(xpos,ypos,width,height)
@@ -11,6 +12,7 @@ class Block(pygame.sprite.Sprite):
         self.image.fill(color)
 
 class Line(pygame.sprite.Sprite):
+    #this class define a line shape sprite with arg :start point,end point
     def __init__(self,start,end,color=(100,100,244),width=1):
         super().__init__()
         self.start=start
@@ -18,10 +20,12 @@ class Line(pygame.sprite.Sprite):
         self.color=color
         self.rect=pygame.Rect(0,0,0,0)
         self.width=width
-    def draw(self,window):
-        self.rect=pygame.draw.line(window,self.color,self.start,self.end,width=self.width)
+    def draw(self,screen):
+        #this function will draw a line on the screen
+        self.rect=pygame.draw.line(screen,self.color,self.start,self.end,width=self.width)
 
 class Circle(pygame.sprite.Sprite):
+    #this class define a circle shape sprite
     def __init__(self, color, radius,pos):
         super().__init__()
         self.image = pygame.Surface([2*radius, 2*radius])
@@ -38,6 +42,15 @@ class Circle(pygame.sprite.Sprite):
 
 
 def if_collide(new_point_x,new_point_y,block_list):
+    """
+        determine if new generated point collide with block
+        arg:
+            new_point_x (usually double)
+            new_point_y (usually double)
+            block_list (pygame sprite group)
+        return:
+            bool (bool)
+    """
     bool=False
     for block in block_list:
         if new_point_x > block.rect.left and  new_point_x < block.rect.right \
@@ -46,6 +59,14 @@ def if_collide(new_point_x,new_point_y,block_list):
     return bool
 
 def find_most_closed_point(point_list,point_in):
+    """
+        find the point on existed path that is most closed to the input point
+        arg:
+            point_list (list) existed path
+            point_in (array) an array contains input point coordinate
+        return:
+            point_out (array) an array contains coordinate of point existed on path
+    """
     norm_min = max(np.abs(point_list[0]-point_in))
     point_out = np.array([0,0])
     for pt in point_list:
@@ -56,36 +77,45 @@ def find_most_closed_point(point_list,point_in):
     return point_out
 
 def RRTstar(block,start,end,step_size,max_itertime,screen,point_list):
+    #use rrt star algorithm to find a path from start point to target point(end)
     path_tree = []
     path_tree.append(start)
     for i in range(max_itertime):
         x_rand = random.randrange(screen.get_width())
         y_rand = random.randrange(screen.get_height())
-        p_rand=np.array([x_rand,y_rand])
-        p_close = find_most_closed_point(path_tree,p_rand)
-        direction = p_rand-p_close
+        point_rand=np.array([x_rand,y_rand])
+        point_close = find_most_closed_point(path_tree,point_rand)
+        direction = point_rand-point_close
         unit_vector = direction / np.linalg.norm(direction)
-        p_new = p_close+step_size*unit_vector
-        #print(p_rand,p_close,p_new)
-        if not if_collide(p_new[0],p_new[1],block):
-            path_tree.append(p_new)
-            draw_path(screen,point_list,p_new,p_close)
-            if max(np.abs(end-p_new))<=step_size:
-                l_new=Line(p_new,end)
-                l_new.draw(screen)
+        point_new = point_close+step_size*unit_vector
+        #print(point_rand,point_close,point_new)
+        if not if_collide(point_new[0],point_new[1],block):
+            path_tree.append(point_new)
+            draw_path(screen,point_list,point_new,point_close)
+            if max(np.abs(end-point_new))<=step_size:
+                line_new=Line(point_new,end)
+                line_new.draw(screen)
                 pygame.display.update()
                 break
     return path_tree
 
-def draw_path(screen,point_list,p_new,p_close):
+def draw_path(screen,point_list,point_new,point_close):
+    #use pygame to display point and line that gnerated by rrt star
     clock = pygame.time.Clock()
-    c_new=Circle((0,0,255),5,p_new)
-    point_list.add(c_new)
-    l_new=Line(p_close,p_new)
+    circle_new=Circle((0,0,255),5,point_new)
+    point_list.add(circle_new)
+    line_new=Line(point_close,point_new)
     point_list.draw(screen)
-    l_new.draw(screen)
+    line_new.draw(screen)
     clock.tick(20) #frames per second
     pygame.display.update()
+    whether_quit()
+
+def whether_quit():
+    #this function use to determine if user push the close bottom of the window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
 
 if __name__ == '__main__': 
     pygame.init()
@@ -113,16 +143,14 @@ if __name__ == '__main__':
     block_list.add(BLOCK1)
     block_list.add(BLOCK2)
     # block_list.add(BLOCK3)
-    #---------------------rrt and display
+    #---------------------rrt star and display
     idx=0
     while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
         screen.fill((0,0,0))
         block_list.draw(screen)
         if idx<1:
             path=RRTstar(block_list,START_P,END_P,30,1000,screen,point_list)
             path.pop(0)
             print(len(path))
-            idx=1       
+            idx=1   
+        whether_quit()    
